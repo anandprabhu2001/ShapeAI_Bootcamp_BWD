1847e711d23d797633b4f9bb6683f6c41c311a56
anandvprabhu <86304727+anandvprabhu@users.noreply.github.com>
2021-06-27 21:19:37 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>No.1 Scientist in the world</h1>
+      <p>
+        Timothy Berners-Lee is a computer scientist, best known as the inventor
+        of the World Wide Web.
+      </p>
+    </div>
+  );
+}
+export default Info;
